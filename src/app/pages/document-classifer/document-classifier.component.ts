3bcd833cd2bdf84ed841b16dcb40dca76cf1ed9f
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2019-01-10 00:44:01 -0500

    integrated aws-js-sdk

--- src/app/pages/document-classifer/document-classifier.component.ts
+++ src/app/pages/document-classifer/document-classifier.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as AWS from 'aws-sdk';
+
 @Component({
   selector: 'ngx-document-classifier',
   templateUrl: './document-classifier.component.html',
@@ -7,12 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DocumentClassifierComponent implements OnInit {
-
+  private url:String;
   constructor() { }
 
   ngOnInit() {
+    // var params = {
+    //   Bucket: 'sagemaker-us-east-2-612969343006',
+    //   Key: 'mykey.txt',
+    //   Body: "HelloWorld"
+    // };
+
+    var s3 = new AWS.S3();
+
+    // s3.putObject(params, function (err, res) {
+    //   if (err) {
+    //       console.log("Error uploading data: ", err);
+    //   } else {
+    //       console.log("Successfully uploaded data to myBucket/myKey");
+    //   }
+    // });
+//
+
+    // var p2 = {
+    //   Bucket:   'sagemaker-us-east-2-612969343006'  }
+
+    // s3.listObjects(p2, function(err,data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else     console.log(data);           // successful response
+    // })
 
 
+    // var s3bucket = s3.listBuckets(function(err, data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else     console.log(data);           // successful response
+    //   /*
+    //   data = {
+    //    Buckets: [
+    //       {
+    //      CreationDate: <Date Representation>, 
+    //      Name: "examplebucket"
+    //     }, 
+    //       {
+    //      CreationDate: <Date Representation>, 
+    //      Name: "examplebucket2"
+    //     }, 
+    //       {
+    //      CreationDate: <Date Representation>, 
+    //      Name: "examplebucket3"
+    //     }
+    //    ], 
+    //    Owner: {
+    //     DisplayName: "own-display-name", 
+    //     ID: "examplee7a2f25102679df27bb0ae12b3f85be6f290b936c4393484be31"
+    //    }
+    //   }
+    //   */
+    // });
+
+
+
+    var params = {
+      Bucket: "sagemaker-us-east-2-612969343006", 
+      Key: "LDA_Visualization.html"
+     };
+     s3.getSignedUrl('getObject', params, function (err, url) {
+      console.log('The URL is', url);
+      });
+    //  s3.getObject(params, function(err, data) {
+    //    if (err) console.log(err, err.stack); // an error occurred
+    //    else     console.log(data);           // successful response
+    //    /*
+    //    data = {
+    //     AcceptRanges: "bytes", 
+    //     ContentLength: 3191, 
+    //     ContentType: "image/jpeg", 
+    //     ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+    //     LastModified: <Date Representation>, 
+    //     Metadata: {
+    //     }, 
+    //     TagCount: 2, 
+    //     VersionId: "null"
+    //    }
+    //    */
+    //  });
   }
 
 }
--- src/app/pages/document-classifer/drop-down-menu/drop-down-menu.component.ts
+++ src/app/pages/document-classifer/drop-down-menu/drop-down-menu.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+import { DomSanitizer, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 
 const URL = 'path_to_api';
+
+
+import * as AWS from 'aws-sdk';
 
 @Component({
   selector: 'ngx-drop-down-menu',
@@ -9,6 +13,7 @@ const URL = 'path_to_api';
   styleUrls: ['./drop-down-menu.component.scss'],
 })
 export class DropDownMenuComponent implements OnInit {
+  ldaURL:any;
 
   public uploader: FileUploader = new FileUploader({url: URL});
   public hasBaseDropZoneOver: boolean = false;
@@ -18,7 +23,29 @@ export class DropDownMenuComponent implements OnInit {
     this.hasBaseDropZoneOver = e;
   }
 
-  constructor() { }
+  constructor(public _sanitationService: DomSanitizer) { 
+
+    // var params = {
+    //   Bucket: 'sagemaker-us-east-2-612969343006',
+    //   Key: 'mykey.txt',
+    //   Body: "HelloWorld"
+    // };
+
+    var s3 = new AWS.S3();
+    
+    var params = {
+      Bucket: "sagemaker-us-east-2-612969343006", 
+      Key: "LDA_Visualization.html"
+     };
+     s3.getSignedUrl('getObject', params, (err, url) => {
+      console.log('The URL is', url);
+      this.ldaURL = url;
+      });
+  }
+
+  cleanURL(oldURL: string): SafeResourceUrl  {
+    return this._sanitationService.bypassSecurityTrustResourceUrl(oldURL);
+   }
 
   ngOnInit() {
   }
